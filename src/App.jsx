4f770785289ebc27afe049a97e89fc5e4830d865
sajan <sajan@Rajans-MacBook-Air.local>
2024-07-30 20:56:30 +0530

    reorganized the folders and files and encapsulated the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,8 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import './App.css'
-import Signupin from './components/Signupin';
-import LandingPage from './components/LandingPage';
+import MedigoLandingPage from "./pages/MedigoLandingPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          exact path='/'
-          element={<LandingPage />}>
-        </Route>
-        <Route
-          exact path='/Signin'
-          element={
-            <Signupin
-              initial={
-                {
-                  state: 1,
-                  switch: {
-                    message: "Don't have an account?",
-                    action: "Sign up"
-                  }
-                }
-              } />
-          }>
-        </Route>
-        <Route
-          exact path='/Signup'
-          element={
-            <Signupin
-              initial={
-                {
-                  state: 0,
-                  switch: {
-                    message: "Already have an account?",
-                    action: "Sign in"
-                  }
-                }
-              } />
-          }>
-        </Route>
-        <Route exact path="*" element={<LandingPage/>} >
-          </Route>
-      </Routes>
-    </Router>
+    <MedigoLandingPage></MedigoLandingPage>
   )
 }
 export default App
