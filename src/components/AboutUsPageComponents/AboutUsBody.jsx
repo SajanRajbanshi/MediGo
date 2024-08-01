6be588dcd39ada5b37d713412b27d8a1381d403e
Utkarsh77218 <saxena2002utkarsh@gmail.com>
2024-08-01 13:09:12 +0530

    modified:   src/components/AboutUsPageComponents/AboutUsBody.jsx

--- src/components/AboutUsPageComponents/AboutUsBody.jsx
+++ src/components/AboutUsPageComponents/AboutUsBody.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import {
+  Box,
+  Container,
+  Typography
+} from '@mui/material';
 
 const AboutUsBody = () => {
   return (
-    <Box sx={{marginTop: '104px'}}>
-      Hey
-    </Box>
+    <Container sx={{ marginTop: '110px' }}>
+      <Box
+        alignItems={'center'}
+        display={'flex'}
+        flexDirection={'column'}
+        height="80vh"
+        justifyContent={'center'}
+        width="50%"
+      >
+        <Typography
+          variant='h2'
+          component='h1'
+          fontWeight={600}
+        >
+          About Medi<span style={{ color: 'red' }}>Go</span>
+        </Typography>
+        <Typography variant='h5' component='h3' sx={{color: '#5c5c5c'}}>
+          hey
+        </Typography>
+      </Box>
+    </Container>
   )
 }
 
